fdf9cb159dd8b393738db94f3af25595e00ada1c
alexbenevides <alex.benevides@hotmail.com>
2016-09-29 23:51:32 -0300

    Checking if the tests is working

---
+++ 04-implementacao/hyperp_app_apresentacao/src/test/java/servico/ServidorTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servico;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class ServidorTest {
+    
+    public ServidorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of iniciarServidor method, of class Servidor.
+     */
+    @Test
+    public void testIniciarServidor() {
+        assertEquals(10, 5+5);
+        
+        /*
+        System.out.println("iniciarServidor");
+        Servidor.iniciarServidor();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        */
+    }
+
+    /**
+     * Test of replicarMensagemEspectador method, of class Servidor.
+     */
+    @Test
+    public void testReplicarMensagemEspectador() throws Exception {
+        
+        /*
+        System.out.println("replicarMensagemEspectador");
+        String mensagem = "";
+        Servidor.replicarMensagemEspectador(mensagem);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        */
+    }
+    
+}
